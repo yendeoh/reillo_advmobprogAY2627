276e665fe5782ae86b7d130b9fde9dc5130699fb
yendeoh <reilloisid@gmail.com>
2026-08-04 22:07:03 +0800

    testing push

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
 # reillo_mobile
 
-A new Flutter project.
+INF231
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+The model defines the structure of the data, the service retrieves the data from the API 
+endpoint, and the screen uses that data to display the information on the user interface. 
+This new design pattern is called the Model–Service–Screen pattern, which makes the app 
+easier to organize, maintain, and update because each part has a clear responsibility.
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Learn Flutter](https://docs.flutter.dev/get-started/learn-flutter)
-- [Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Flutter learning resources](https://docs.flutter.dev/reference/learning-resources)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+# lab act 2 test
--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,42 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ThemeProvider with ChangeNotifier {
-  final Color _customPrimaryColor = const Color(0xFF222B6F);
   bool _isDark = false;
   bool get isDark => _isDark;
 
-  MaterialColor _createMaterialColor(Color color) {
-    final swatch = <int, Color>{
-      50: color.withOpacity(0.1),
-      100: color.withOpacity(0.2),
-      200: color.withOpacity(0.3),
-      300: color.withOpacity(0.4),
-      400: color.withOpacity(0.5),
-      500: color.withOpacity(0.6),
-      600: color.withOpacity(0.7),
-      700: color.withOpacity(0.8),
-      800: color.withOpacity(0.9),
-      900: color,
-    };
-
-    return MaterialColor(color.value, swatch);
-  }
-
   ThemeData get lightTheme => ThemeData(
     brightness: Brightness.light,
-    primarySwatch: _createMaterialColor(_customPrimaryColor),
+    primarySwatch: Colors.blue,
     scaffoldBackgroundColor: Colors.grey[100],
-    appBarTheme: AppBarTheme(
-      backgroundColor: _customPrimaryColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.blue,
       foregroundColor: Colors.white,
     ),
   );
 
   ThemeData get darkTheme => ThemeData(
     brightness: Brightness.dark,
-    primarySwatch: _createMaterialColor(_customPrimaryColor),
+    primarySwatch: Colors.blue,
     scaffoldBackgroundColor: Colors.grey[900],
-    appBarTheme: AppBarTheme(
+    appBarTheme: const AppBarTheme(
       backgroundColor: Colors.black,
       foregroundColor: Colors.white,
     ),
